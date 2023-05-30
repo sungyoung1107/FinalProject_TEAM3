8e63b057b844a6d6f8cbf502e177e831403192a3
JINGENIE <geniejin96@gmail.com>
2023-05-30 17:32:20 +0900

    0530

--- src/test/java/com/kbstar/Gym/SelectTest.java
+++ src/test/java/com/kbstar/Gym/SelectTest.java
@@ -1,6 +1,6 @@
 package com.kbstar.Gym;
 
-import com.kbstar.service.CustService;
+import com.kbstar.service.GymService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,17 +8,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @Slf4j
 @SpringBootTest
-class SelectTest {
+public class SelectTest {
     @Autowired
-    CustService service;
+    GymService service;
     @Test
-    void contextLoads() {
-        try {
+    void contextLoads(){
+        try{
             service.get();
-        } catch (Exception e) {
-            log.info("에러..");
-            //e.printStackTrace();
+        }catch (Exception e) {
+            log.info("error!!!");
         }
     }
-
 }
